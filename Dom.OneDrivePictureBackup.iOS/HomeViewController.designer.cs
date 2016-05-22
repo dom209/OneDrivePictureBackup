0889dd1ea265253fcb4aca7cf816a26947c5fb2b
Dominic Reid <dominic.reid@dmar.me.uk>
2016-05-22 23:05:02 +0100

    Added One Drive SDK nuget package.

--- Dom.OneDrivePictureBackup.iOS/HomeViewController.designer.cs
+++ Dom.OneDrivePictureBackup.iOS/HomeViewController.designer.cs
@@ -1,10 +1,13 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
+// This file has been generated automatically by Xamarin Studio from the outlets and
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace Dom.OneDrivePictureBackup.iOS
 {
